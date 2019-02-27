d4c877d3cc11d9d4400c9eda10790f753d698881
Shabab Ayub <shababayub@gmail.com>
2019-02-26 20:06:40 -0500

    safely append neighbours

--- software/kowloon/path.py
+++ software/kowloon/path.py
@@ -21,12 +21,30 @@ def a_star(grid, start, goal):
         if current == goal:
             break
 
-        for next in graph.neighbors(current):
-            new_cost = cost_so_far[current] + graph.cost(current, next)
-            if next not in cost_so_far or new_cost < cost_so_far[next]:
-                cost_so_far[next] = new_cost
-                priority = new_cost + heuristic(goal, next)
-                frontier.put(next, priority)
-                came_from[next] = current
+        for nxt in neighbors(grid, current):
+            new_cost = cost_so_far[current] + grid.cost(current, nxt)
+            if n not in cost_so_far or new_cost < cost_so_far[nxt]:
+                cost_so_far[nxt] = new_cost
+                priority = new_cost + heuristic(goal, nxt)
+                frontier.put(nxt, priority)
+                came_from[nxt] = current
 
     return came_from, cost_so_far
+
+def neighbours(grid, pos):
+    n = []
+    top_boundary = 0
+    left_boundary = 0
+    bottom_boundary = len(grid) - 1
+    right_boundary = len(grid[0]) - 1
+
+    if pos[1] > top_boundary: # above
+        n.append([pos[0], pos[1]-1])
+    if pos[1] < bottom_boundary: # below
+        n.append([pos[0], pos[1]+1])
+    if pos[0] > left_boundary: #left
+        n.append([pos[0]-1, pos[1]])
+    if pos[0] < right_boundary: # right
+        n.append([pos[0]+1, pos[1]])
+
+    return n
