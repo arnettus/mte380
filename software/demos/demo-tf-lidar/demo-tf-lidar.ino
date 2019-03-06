3d946638d2262292bb63c06da26c41c6a6dd2bfa
Shabab Ayub <shababayub@gmail.com>
2019-03-06 18:32:14 -0500

    we changed to polling cuz we though isr was bad, but nahhh`

--- software/demos/demo-tf-lidar/demo-tf-lidar.ino
+++ software/demos/demo-tf-lidar/demo-tf-lidar.ino
@@ -4,21 +4,34 @@ TFLidar lidar(8);
 
 void setup() {
     Serial.begin(9600);
-    lidar.begin(&Serial1);
+    lidar.begin(&Serial3);
 
-    OCR0A = 0x3F;   // approximately 4x a ms
-    TIMSK0 |= _BV(OCIE0A);
+    //OCR0A = 0x7F;   // approximately 4x a ms
+    //TIMSK0 |= _BV(OCIE0A);
 }
-
+/*
 volatile bool pollMe = false;
 
 ISR(TIMER0_COMPA_vect) {
     if (!pollMe)
         pollMe = true;
 }
-
+*/
+unsigned long oldTime = 0;
 void loop() {
-    if (pollMe) {
+      unsigned long newTime = millis();
+      
+      if ((newTime - oldTime) > 5) {
+          TFStatus st = lidar.measure();
+          if (st == TF_STATUS_OKAY) {
+              lidar.printLatest(&Serial, true);
+          } else if (st != TF_STATUS_ERROR_BAD_CHECKSUM) {
+              Serial.print("Error code: ");
+              Serial.println(st);
+          }
+          oldTime = newTime;
+      }
+/*    if (pollMe) {
         pollMe = false;
         TFStatus st = lidar.measure();
 
@@ -27,6 +40,7 @@ void loop() {
         } else if (st != TF_STATUS_ERROR_BAD_CHECKSUM) {
             Serial.print("Error code: ");
             Serial.println(st);
-        }       
-    }
+        }
+    }*/
+    
 }
