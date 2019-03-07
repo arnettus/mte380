fe95f04e32e43aa79710e06f0493fd46cadb15fe
Shabab Ayub <shababayub@gmail.com>
2019-03-07 00:43:00 -0500

    updated flame

--- software/libraries/Flame/Flame.h
+++ software/libraries/Flame/Flame.h
@@ -2,14 +2,6 @@
 #define FLAME_H
 
 #include <stdio.h>
-#include <Arduino.h>
-
-enum FlameStatus {
-    FLAME_NOT_FOUND,
-    FLAME_IS_LEFT,
-    FLAME_IS_RIGHT,
-    FLAME_IS_CENTERED,
-};
 
 class Flame {
 private:
@@ -19,8 +11,7 @@ private:
 public:
     Flame(int _leftPin, int _rightPin);
 
-    int readValue(bool useLeft);
-    FlameStatus locate();
+    bool isFlameInSight();
 };
 
 #endif
