c43d6ee6f4981db42ecef56771dcfbdeb61dd83e
Shabab Ayub <shababayub@gmail.com>
2019-03-15 01:19:43 -0400

    shabab's adjustments

--- software/demos/demo-colour/demo-colour.ino
+++ software/demos/demo-colour/demo-colour.ino
@@ -1,12 +1,14 @@
 #include "Colour.h"
 
-Colour colour(28, 26, 24, 22, 30, Colour::Frequency::PERCENT_20);
+Colour colour(4, 7, 8, 11, 13, Colour::Frequency::PERCENT_20);
 void setup() {
     Serial.begin(9600);
+    pinMode(2, OUTPUT);
+    digitalWrite(2, HIGH);
 }
 
 unsigned long frequency;
 
-void loop() {
+void loop() { 
     colour.PrintRGB();
 }
