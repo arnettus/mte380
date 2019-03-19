859609ecbb9746ab466d0916f7f95ba9102783f1
Shabab Ayub <shababayub@gmail.com>
2019-03-19 18:29:44 -0400

    add default constructor to coordinate

--- software/libraries/Coordinate/Coordinate.h
+++ software/libraries/Coordinate/Coordinate.h
@@ -1,8 +1,11 @@
 #ifndef COORDINATE_H
 #define COORDINATE_H
 
+const int OFF_THE_MAP = -100;
+
 class Coordinate {
     public:
+        Coordinate();
         Coordinate(int _x, int _y);
         int x;
         int y;
@@ -10,6 +13,7 @@ class Coordinate {
         bool operator==(Coordinate rhs);
 };
 
+Coordinate::Coordinate() : x(OFF_THE_MAP), y(OFF_THE_MAP) {}
 Coordinate::Coordinate(int _x, int _y) : x(_x), y(_y) {}
 
 bool Coordinate::operator==(Coordinate rhs) {
